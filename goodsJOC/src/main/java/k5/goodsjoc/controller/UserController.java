ad69e9a384230de816c27659689013eee621f792
jeongdohye <93048497+jeongdohye@users.noreply.github.com>
2022-01-12 10:53:52 +0900

    [good JOC] 정도혜 수정완료

--- goodsJOC/src/main/java/k5/goodsjoc/controller/UserController.java
+++ goodsJOC/src/main/java/k5/goodsjoc/controller/UserController.java
@@ -22,78 +22,6 @@ import k5.goodsjoc.dto.User;
 @Controller
 @RequestMapping(value= "/system_management/user")
 public class UserController {
-<<<<<<< HEAD
-   private static final Logger log = LoggerFactory.getLogger(UserController.class);
-   private MartService martService;
-   private UserService userService;
-   public UserController(UserService userService, MartService martService) {
-      this.userService = userService;
-      this.martService = martService;
-   }
-   
-   @GetMapping("/logout")
-   public String logout(HttpSession session) {
-      session.invalidate();
-      System.out.println("로그아웃 완료");
-      
-      return "redirect:/system_management/user/loginForm";
-   }
-   
-   @PostMapping("/login")
-   public String login(@RequestParam(value="ID", required=false) String ID,
-                  @RequestParam(value="PW", required=false) String PW, HttpSession session) {
-      System.out.println("페이지: 로그인처리 ");
-      System.out.println("경로: system_management/user/loginForm(POST방식) ");      
-      System.out.println("입력받은 ID: " + ID);
-      System.out.println("입력받은 PW: " + PW);
-      
-      if(ID != null && !"".equals(ID) && PW != null && !"".equals(PW)){
-         User userInfo = userService.getUserInfoByID(ID);
-         System.out.println("userInfo: " + userInfo);
-         Mart martInfo = martService.getMartInfoByMartCode(userInfo.getMartCode());
-         System.out.println("martInfo: " + martInfo);
-         
-         if(userInfo != null && userInfo.getPW() != null && PW.equals(userInfo.getPW())) {
-            session.setAttribute("SID", userInfo.getID());
-            session.setAttribute("SNAME", userInfo.getName());
-            session.setAttribute("SMARTCODE", martInfo.getMartCode());
-            session.setAttribute("SMARTNAME", martInfo.getMartName());
-            session.setAttribute("SLEVLEL", userInfo.getLevelNum());
-            session.setAttribute("SPHONE", userInfo.getPhone());
-            if(userInfo.getLevelNum().equals("1")) {
-               session.setAttribute("SLEVELNAME", "SW개발사");
-            }else if(userInfo.getLevelNum().equals("2")) {
-               session.setAttribute("SLEVELNAME", "관리자");
-            }else if(userInfo.getLevelNum().equals("3")) {
-               session.setAttribute("SLEVELNAME", "매니저");
-            }else if(userInfo.getLevelNum().equals("4")) {
-               session.setAttribute("SLEVELNAME", "직원");
-            }else if(userInfo.getLevelNum().equals("5")) {
-               session.setAttribute("SLEVELNAME", "비권한자");
-            }
-            System.out.println("로그인 성공");
-            return "redirect:/main";   
-         }
-         
-      }
-      System.out.println("로그인 실패");
-      return "system_management/user/loginForm";
-   }
-   
-   
-   @GetMapping("/loginForm")
-   public String loginForm() {
-      System.out.println("페이지: 로그인 화면 ");
-      System.out.println("경로: system_management/user/loginForm(GET방식 성공) ");
-      
-      return "system_management/user/loginForm";
-   }
-   
-   @GetMapping("/userList")
-   public String staffList(HttpServletRequest request, Model model) {
-      System.out.println("페이지: 사용자 목록 ");
-      System.out.println("경로: system_management/user/userList(GET방식 성공) ");
-=======
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	private MartService martService;
 	private UserService userService;
@@ -164,7 +92,6 @@ public class UserController {
 	public String staffList(HttpServletRequest request, Model model) {
 		System.out.println("페이지: 사용자 목록 ");
 		System.out.println("경로: system_management/user/userList(GET방식 성공) ");
->>>>>>> refs/remotes/origin/develop
 
       HttpSession session = request.getSession();
       String sessionMartCode = (String) session.getAttribute("SMARTCODE");
@@ -242,26 +169,26 @@ public class UserController {
    
      //사원권한 수정화면     
      @GetMapping("/userUpdate") 
-     public String userUpdate(@RequestParam(value="ID", required = false) String ID, Model model) {
+     public String userUpdate(@RequestParam(value="id", required = false) String id, Model model) {
      System.out.println("페이지: 사원권한수정 ");
      System.out.println("경로: system_management/user/userUpdate(GET방식 성공) "); 
-     System.out.println("리트스에서 받은 거래처 코드: " + ID);
+     System.out.println("리트스에서 받은 거래처 코드: " + id);
      
-     User userUpdate = userService.getUserInfoByID(ID);
+     User userUpdate = userService.getUserInfoByID(id);
      model.addAttribute("userUpdate", userUpdate);
      System.out.println("모델에 담긴 비즈니스정보: " + model);
      
      return "system_management/user/userUpdate";}
      
      //사원권한 수정 작업
-     @PostMapping("/userUpdate") 
-     public String userUpdate(User user) 
+     @PostMapping("/userLevelUpdate") 
+     public String userLevelUpdate(User user) 
      { System.out.println("페이지: 사원권한 수정 ");
-     System.out.println("경로: system_management/user/userUpdate(POST방식 성공) "); 
+     System.out.println("경로: system_management/user/userLevelUpdate(POST방식 성공) "); 
      log.info("수정화면에서 받은 userUpdate : {}", user);
      
-     userService.updateUser(user);
+     userService.updateUserLevel(user);
      
-     return "redirect:/system_management/user/userUpdate";}
+     return "redirect:/system_management/user/userList";}
     
 }
