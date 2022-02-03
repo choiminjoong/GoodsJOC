bbf62b86afeda5e290875f7eae93f8316ba0e92e
Minjoong Choi <96754003+choiminjoong@users.noreply.github.com>
2022-02-03 10:04:38 +0900

    [Goods JOC] CMJ => develop push 2022-02-03 10:04

--- goodsJOC/src/main/java/k5/goodsjoc/service/BusinessService.java
+++ goodsJOC/src/main/java/k5/goodsjoc/service/BusinessService.java
@@ -37,29 +37,15 @@ public class BusinessService {
 	public Business getBusinessInfoByCode(String businessCode) {		
 		return businessMapper.getBusinessInfoByCode(businessCode);
 	}
-<<<<<<< HEAD
-	
+
 	//05.거래처 정보 수정처리
-=======
-		
-	// 거래처 조건 검색
-	public List<Business> getBusinessListBySearchKey(String searchKey, String searchValue, String startDt, String endDt) {
-		return businessMapper.getBusinessListBySearchKey(searchKey, searchValue, startDt, endDt);
-	}
-	
-	// 거래처 정보 수정
->>>>>>> refs/heads/develop
-	public int updateBusinessInfo(Business business) {	
+	public int updateBusinessInfo(Business business) {
 		return businessMapper.updateBusinessInfo(business);
 	}
-<<<<<<< HEAD
-		
-=======
 	
 	//거래처 삭제
 	public int businessDeleteAction(String businessCode) {
 		return businessMapper.businessDeleteAction(businessCode);
 	}
 
->>>>>>> refs/heads/develop
 }
